bf58222925af39e8bb32bfc6eb0e23d84fb78a4b
ElianaCepeda <eli.cepedagonzalez@gmail.com>
2025-02-25 11:57:12 -0500

    springboot

---
+++ juego_caravana_medieval/src/test/java/co/edu/javeriana/juego_caravana_medieval/CaravanaMedievalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.juego_caravana_medieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaravanaMedievalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
