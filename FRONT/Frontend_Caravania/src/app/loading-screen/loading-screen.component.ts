d22f7e17ea8ee38e9c6ba377dbe2b8880db46bba
David <dafelipe0410@gmail.com>
2025-04-01 16:56:25 -0500

    Cambio de imagenes entre loading screen

--- FRONT/Frontend_Caravania/src/app/loading-screen/loading-screen.component.ts
+++ FRONT/Frontend_Caravania/src/app/loading-screen/loading-screen.component.ts
@@ -1,40 +1,47 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-loading-screen',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './loading-screen.component.html',
-  styleUrl: './loading-screen.component.css'
+  styleUrls: ['./loading-screen.component.css']
 })
-export class LoadingScreenComponent {
+export class LoadingScreenComponent implements OnInit {
 
-  currenLoad= '../../assets/img/loading/barra-carga.png';
+  currenLoad = '../../assets/img/loading/barra-carga.png';
+
+  backgroundImage: string = '';
 
   imagenes = [
-    {
-      img: '../../assets/img/loading/barra-carga.png'
-    },
-    {
-      img: '../../assets/img/loading/barra-carga.png'
-    },
-    {
-      img: '../../assets/img/loading/barra-carga.png'
-    }
+    { img: '../../assets/img/loading/fondo1.png' },
+    { img: '../../assets/img/loading/fondo2.png' },
+    { img: '../../assets/img/loading/fondo3.png' },
+    { img: '../../assets/img/loading/fondo4.png' }
   ];
 
   nextRoute: string = '';
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    const randomImg = this.imagenes[Math.floor(Math.random() * this.imagenes.length)].img;
+
+    // Construye el background completo (gradiente + imagen aleatoria)
+    this.backgroundImage = `
+    linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)),
+    url('${randomImg}')
+  `;
+
+
+
     this.nextRoute = this.route.snapshot.queryParamMap.get('next') || '/welcome';
-    
+
     setTimeout(() => {
       this.router.navigateByUrl(this.nextRoute);
-    }, 3500);
+    }, 3500); // redirige después de 3.5s
   }
-
-
-
 }
